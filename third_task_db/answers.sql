291c40d95601ad39a9341593854b025c0fd1c2d3
cserneczkybalint <cserneczkybalint@gmail.com>
2020-11-25 13:11:53 +0100

    3rd task 5,6

--- third_task_db/answers.sql
+++ third_task_db/answers.sql
@@ -1,8 +1,8 @@
 1. listázd azon userek nevét, akiknek nincs autója és a vezetékneve Kis :
-SELECT user.name FROM `user`,user_car WHERE user_car.user = user.id and name LIKE "Kis%" GROUP BY user_car.user HAVING COUNT(user_car.user) = 0
+SELECT user.name FROM `user`,user_car WHERE user_car.user = user.id and name LIKE "Kis%" GROUP BY user_car.user HAVING COUNT(user_car.user) = 0;
 
 2.listázd azon userek nevét, akiknek legalább 2 autója van	
-SELECT user.name FROM `user`,user_car WHERE user_car.user = user.id GROUP BY user_car.user HAVING COUNT(user_car.user) > 1
+SELECT user.name FROM `user`,user_car WHERE user_car.user = user.id GROUP BY user_car.user HAVING COUNT(user_car.user) > 1;
 
 2.1
 backlog
@@ -17,3 +17,15 @@ ADD nem BIT;
 4.
 INSERT INTO `car`(`brand`, `model`) VALUES ("Volkswagen","Arteon")
 -- AI miatt nem kell id-t kitölteni
+
+5. módosítsd a car tábla tartalmában azokat a model-eket Fiesta-ra, ahol a model Focus
+UPDATE `car` SET model="Fiesta" WHERE model="Focus";
+
+6. adj hozzá minden userhez egy volkswagen arteont akinek a nevében szerepel "o" vagy "r" betű illetve az id-ja kisebb, mint 10	
+
+INSERT INTO `user_car`(`user`, `car`) 
+    SELECT id,(SELECT id FROM `car` where brand="Volkswagen" and model="Arteon")
+                     FROM `user` where id < 10 and (name LIKE "%o%" or name LIKE "%r%")
+
+7.biztosítsd a user_car tábla egyediségét index segítségével	
+
